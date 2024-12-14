af9fd67228c16a81ad2d490e4bb5d6b0e671b60e
phongdang1 <phongdbde160574@fpt.edu.vn>
2024-12-14 19:25:13 +0700

    4

--- vite.config.js
+++ vite.config.js
@@ -16,7 +16,7 @@ export default defineConfig({
   server: {
     proxy: {
       "/auth/google": {
-        target: "http://localhost:5000",
+        target: "https://be-jobfinder111.onrender.com",
         changeOrigin: true,
         secure: false,
       },
